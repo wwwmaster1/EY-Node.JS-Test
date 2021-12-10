eaebe265077c183f286fd4d8615d1bb25a5efdc3
Igor Ilyinsky <github@maverickmaven.com>
2021-12-09 18:14:27 -0700

    Creating App.JS
    This is the complete file as run with node.

---
+++ app.js
@@ -0,0 +1,42 @@
+const https = require('https');
+
+var options = {
+  host: 'api.github.com',
+  path: '/orgs/google/repos',
+  method: 'GET',
+  headers: {'user-agent': 'node.js'}
+};
+
+https.get(options, (resp) => {
+  let data = '';
+  let cntr = 0;
+  let langs = [];
+
+  resp.on('data', (chunk) => { data += chunk; });
+
+  resp.on('end', () => {    
+
+    for(var i = 0; i < JSON.parse(data).length; i++)
+    {
+      langs[i] = JSON.parse(data)[i].language;
+    }
+    langs.sort();
+    for(var i = 0; i < langs.length; i++)
+    {
+      if (i == 0){
+        console.log(langs[i]);
+        cntr = 1;
+      } else if (langs[i] != langs[i-1] ){
+        console.log(cntr);
+        console.log(langs[i]);
+        cntr = 1;
+      } else {
+        cntr++;
+      }
+    }
+
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
